60bc69229ea65de4f241398f84e99302a8cbe9d8
Breno Inove <breno@inove9.com>
2023-10-25 10:48:15 -0300

    Colocando data e hora antes do nome do arquivo

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -8,7 +8,16 @@ export default {
   storage: multer.diskStorage({
     destination: publicFolder,
     filename(req, file, cb) {
-      return cb(null, file.originalname);
+	const data = new Date();
+	const dia = data.getDate().toString().padStart(2, '0');
+  	const mes = (data.getMonth() + 1).toString().padStart(2, '0');
+  	const ano = data.getFullYear();
+  	const hora = data.getHours().toString().padStart(2, '0');
+  	const minuto = data.getMinutes().toString().padStart(2, '0');
+
+	const nomeDoArquivo = `${dia}-${mes}-${ano}-${hora}-${minuto}-${file.originalname}`;
+
+        return cb(null,nomeDoArquivo);
     }
   })
 };
